f23b06d70ac7d54282a971fdb0a71658eb4dec17
Angela Vuong <vuong.angela@gmail.com>
2020-08-14 15:43:43 -0400

    HuffmanNodes creates a new instance and a tree node and getAllNodes creates nodes for all chars in string

--- huffmanTree.js
+++ huffmanTree.js
@@ -4,4 +4,17 @@ const freqHash = compressionHashTable.frequencyHashTable(
   'the quick brown fox jumped over the fence'
 );
 
-console.log(freqHash);
+function HuffmanNodes(char, count) {
+  this.char = char;
+  this.count = count;
+}
+
+function getAllNodes(obj) {
+  let allNodes = [];
+  for (let keys in obj) {
+    allNodes.push(new HuffmanNodes(keys, obj[keys]));
+  }
+  return allNodes;
+}
+
+console.log(getAllNodes(freqHash));
