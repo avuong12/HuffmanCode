6e10233066b9fd97bcf891a2edc49a933ea06a92
Angela Vuong <vuong.angela@gmail.com>
2020-08-14 15:08:58 -0400

    created character frequency hash table function

---
+++ compression-hash-table.js
@@ -0,0 +1,14 @@
+// Map characters in T to the frequency the character appares in T.
+function frequencyHashTable(str) {
+  const charsFrequency = {};
+  for (let i = 0; i < str.length; i++) {
+    if (charsFrequency.hasOwnProperty(str[i])) {
+      charsFrequency[str[i]] = charsFrequency[str[i]] + 1;
+    } else {
+      charsFrequency[str[i]] = 1;
+    }
+  }
+  return charsFrequency;
+}
+
+console.log(frequencyHashTable('the quick brown fox jumped over the fence'));
